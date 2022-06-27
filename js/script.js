666e8c020a26384e4f8eeadb5f4f025d33c2586c
Your Name <you@example.com>
2022-06-26 22:54:35 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('button');
+let p = document.querySelector('p');
+
+btn.addEventListener('click', () => {
+    // console.log('Hello');
+    // alert('Hello!');
+    p.innerText = 'Hello!';
+});
